66ea1a12aa84ac194fbdc3be34451c243e5dd396
Paul Edmiston <paul@edmiston.org>
2022-10-16 12:45:02 -0700

    Multi-element button testing, and script integration, successful. Font importing. Defining CSS variables.

---
+++ ~_scripts/main.js
@@ -0,0 +1,9 @@
+let showcaseGallery = document.getElementById("showcaseGallery");
+let multilineTest = `<button type="button">
+	<div>NEW</div>
+	<img src="!_placeholder1/chirp.png" alt="">
+	<div>This is a button with multiple interior elements.</div>
+</button>`;
+
+
+showcaseGallery.innerHTML = multilineTest;
