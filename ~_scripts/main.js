7bbdf75cca4ce85e90ca3b35ff90882f876b1146
Paul Edmiston <paul@edmiston.org>
2022-10-16 13:44:44 -0700

    Progress toward button placement.

--- ~_scripts/main.js
+++ ~_scripts/main.js
@@ -1,4 +1,39 @@
-let showcaseGallery = document.getElementById("showcaseGallery");
+const todaysDate = new Date();
+const folderBlacklist = [
+	"~_css", 
+	"~_scripts"
+];
+
+/* This would ideally be executed with something more automatic, but according to my research, this requires some back-end functionality (eg. Node.JS or PHP.) The former would require some back-end setup that I don't have the authority in my server setup to do nor the time for. The latter is available, but making PHP and JS play nice with each other feels dirty.
+I'll have to settle for a student array and do some extra clean-up from here. Fortunately I have a consistent naming system going on to make this work. If you end up doing things differently, however, expect to revisit this. */
+const students = [
+	"!_paulEdmiston",
+	"!_placeholder1",
+	"!_placeholder2",
+	"!_placeholder3",
+	"abrahamSegura",
+	"anelleSantana",
+	"brittanyAcceturo",
+	"cheloyGonzalez",
+	"christopherFelix",
+	"christopherTa",
+	"davidCummiskey",
+	"farrahLam",
+	"jacobVerduzco",
+	"jorgeMRocha",
+	"juneKwon",
+	"leksiaTorres",
+	"milagrosPerez",
+	"mollyBolt",
+	"nayelliZechman",
+	"rebeccaTang",
+	"sharonCho",
+	"tinaHu",
+	"vickyShan",
+	"zenaXayalath"
+]
+
+const showcaseGallery = document.getElementById("showcaseGallery");
 let multilineTest = `<button type="button">
 	<div>NEW</div>
 	<img src="!_placeholder1/chirp.png" alt="">
