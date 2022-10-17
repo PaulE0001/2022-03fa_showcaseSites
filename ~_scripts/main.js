110331684c0020b1a63437a7afe08bb55518be66
Paul Edmiston <paul@edmiston.org>
2022-10-17 12:12:51 -0700

    "Let's go Morty, install jQuery. Quick update and restart. 20 minute adventure." it was not a 20 minute adventure.

--- ~_scripts/main.js
+++ ~_scripts/main.js
@@ -6,7 +6,7 @@ const folderBlacklist = [
 
 /* This would ideally be executed with something more automatic, but according to my research, this requires some back-end functionality (eg. Node.JS or PHP.) The former would require some back-end setup that I don't have the authority in my server setup to do nor the time for. The latter is available, but making PHP and JS play nice with each other feels dirty.
 I'll have to settle for a student array and do some extra clean-up from here. Fortunately I have a consistent naming system going on to make this work. If you end up doing things differently, however, expect to revisit this. */
-const students = [
+let students = [
 	"!_paulEdmiston",
 	"!_placeholder1",
 	"!_placeholder2",
@@ -42,3 +42,17 @@ let multilineTest = `<button type="button">
 
 
 showcaseGallery.innerHTML = multilineTest;
+
+
+let buttonImg;
+let config;
+let description;
+
+/* The above elements are essential for how the showcase is constructed. If one of the above is not present in the student's folder (ie. value undefined), we can assume that the student's project is not ready for display. Two functions are used toward this end. A general purpsoe file loader lets us type that only once rather than thrice. Then the `studentCleanup` function actually does the deed. */
+function getFile(filePath) {
+	return $.get(filePath);
+}
+function studentCleanup(value, index, array) {
+	
+}
+students.forEach(studentCleanup);
